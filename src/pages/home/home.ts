5cb098874ffad5ee556d819a576a62ecb47033e6
eliot <etudiant@wificampus-045242.grenet.fr>
2017-02-14 16:15:49 +0100

    ajout implémentation api chemin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
  
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-
 export class HomePage {
  
   posts: any;
  
-  constructor() {
+  constructor(public http: Http) {
+ 
+    this.http.get('http://checkin-api.dev.cap-liberte.com/checkin').map(res => res.json()).subscribe(id => {
+        this.posts = id;
+    });
  
   }
 }
